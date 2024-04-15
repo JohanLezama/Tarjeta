e285ed057c9ceefbcee93b2eea9de22a4cb55a90
JohanLezama <167055040+JohanLezama@users.noreply.github.com>
2024-04-14 23:35:44 -0600

    Add files via upload

--- java.js
+++ java.js
@@ -1,10 +1,9 @@
-var boton = document.getElementById('velocidad');
-boton.addEventListener('click', calcular);
+var boton = document.getElementById('btnCalcular');
+boton.addEventListener ('click', calcular);
 
 function calcular(){
-    var distancia = parseInt(document.getElementById('distancia').value);
-    var tiempo = parseInt(document.getElementById('tiempo').value);
-    var final = distancia/tiempo
-    document.getElementById('finalvelocity').innerHTML = final;
-
+    var precio = parseInt(document.getElementById('precio').value);
+    var descuento = parseInt(document.getElementById('descuento').value);
+    var resultado = precio - (precio * (descuento/100));
+    document.getElementById('precioFinal').innerHTML = resultado;
 }
